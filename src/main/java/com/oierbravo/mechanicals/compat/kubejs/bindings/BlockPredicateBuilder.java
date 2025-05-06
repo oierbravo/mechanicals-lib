b4751cfd4e254656f1ab687921442dd0c3de1b3e
Oier Bravo Urtasun <oierbravo@gmail.com>
2025-05-06 23:33:57 +0200

    clean up

--- src/main/java/com/oierbravo/mechanicals/compat/kubejs/bindings/BlockPredicateBuilder.java
+++ src/main/java/com/oierbravo/mechanicals/compat/kubejs/bindings/BlockPredicateBuilder.java
@@ -2,16 +2,12 @@ package com.oierbravo.mechanicals.compat.kubejs.bindings;
 
 import com.oierbravo.mechanicals.foundation.util.BlockPredicateUtils;
 import net.minecraft.advancements.critereon.BlockPredicate;
-import net.minecraft.advancements.critereon.StatePropertiesPredicate;
 
 public class BlockPredicateBuilder {
     public static BlockPredicate of(String id){
         return BlockPredicateUtils.Builder.build(id);
     }
-    public static BlockPredicate of(String id, StatePropertiesPredicate stateProperties){
-        return BlockPredicateUtils.Builder.build(id);
-    }
-    //StatePropertiesPredicate.Builder StatePropertiesPredicate
+
     public static BlockPredicate.Builder from(String id){
         return BlockPredicateUtils.Builder.from(id);
     }
